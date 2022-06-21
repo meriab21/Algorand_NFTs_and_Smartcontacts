10fe05e08c20e4e395c59af5bff8f83e9610ade6
meriab21 <meriab1234@gmail.com>
2022-06-21 19:34:41 +0300

    created new file

--- back_end/example.py
+++ back_end/example.py
@@ -1,9 +1,8 @@
 import json
 import base64
-from algosdk import account, mnemonic
+from algosdk import account, mnemonic, constants
 from algosdk.v2client import algod
 from algosdk.future import transaction
-from algosdk import constants
 
 
 def generate_algorand_keypair():
@@ -12,28 +11,24 @@ def generate_algorand_keypair():
     print("My private key: {}".format(private_key))
     print("My passphrase: {}".format(mnemonic.from_private_key(private_key)))
 
+
+# My address: B43RGX7HVTVCXUN5GMCUNLA6X6Y7NEOOZZNF5NYR2A4CPOSXZZJ3VPG3PA
+# My private key: +EwYWXmA2RflodQt/cqZKuHFS9ROc6n1UHt+0lfcDvsPNxNf56zqK9G9MwVGrB6/sfaRzs5aXrcR0DgnulfOUw==
+# My passphrase: pair gentle prosper always reason echo august start pizza cloud erupt bargain mesh base issue infant release duck diet child quick require such abstract exclude
+
 # generate_algorand_keypair()
 
-# My address: 22RSRFQPE6PFOGG7CPKAZ2LWV2H5IZDIVK24T2OEWLSZY7GAKSWKITNAC4
-# My private key: bFiLvLhJsIec0+Ryo92jPzNYuHTJekPY0/lard+0qX7WoyiWDyeeVxjfE9QM6Xauj9RkaKq1yenEsuWcfMBUrA==
-# My passphrase: assault coin furnace oppose gauge capable ostrich tooth hour isolate spin crop race come entry kiss anchor exhaust language stick satisfy honey vocal about envelope
-
-
-# second account created by accident
-# My address: 5OEVG24AWN4PSYJ4KNUNAVRI5AURXISD5BTOAVLBRJUCUP3F5U2MDHTE4U
-# My private key: fAQ0ezrUxNsPSj39dYi6CGD7W0pCP4wEN2z/jFiEb1LriVNrgLN4+WE8U2jQVijoKRuiQ+hm4FVhimgqP2XtNA==
-# My passphrase: moon source diesel dry meadow want expire diary sausage canyon front above walnut notice banner dismiss muscle swing render soup rain three nature able boss
 
 def first_transaction_example(private_key, my_address):
     algod_address = "http://localhost:4001"
     algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     algod_client = algod.AlgodClient(algod_token, algod_address)
 
+    print("My address: {}".format(my_address))
     account_info = algod_client.account_info(my_address)
-    print("Account balance: {} microAlgos".format(
-        account_info.get('amount')) + "\n")
+    print("Account balance: {} microAlgos".format(account_info.get('amount')))
 
-# build transaction
+    # build transaction
     params = algod_client.suggested_params()
     # comment out the next two (2) lines to use suggested fees
     params.flat_fee = constants.MIN_TXN_FEE
@@ -41,6 +36,7 @@ def first_transaction_example(private_key, my_address):
     receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA"
     amount = 100000
     note = "Hello World".encode()
+
     unsigned_txn = transaction.PaymentTxn(
         my_address, params, receiver, amount, None, note)
 
@@ -49,7 +45,7 @@ def first_transaction_example(private_key, my_address):
 
     # submit transaction
     txid = algod_client.send_transaction(signed_txn)
-    print("Successfully sent transaction with txID: {}".format(txid))
+    print("Signed transaction with txID: {}".format(txid))
 
     # wait for confirmation
     try:
@@ -63,6 +59,7 @@ def first_transaction_example(private_key, my_address):
         json.dumps(confirmed_txn, indent=4)))
     print("Decoded note: {}".format(base64.b64decode(
         confirmed_txn["txn"]["txn"]["note"]).decode()))
+
     print("Starting Account balance: {} microAlgos".format(
         account_info.get('amount')))
     print("Amount transfered: {} microAlgos".format(amount))
@@ -73,5 +70,6 @@ def first_transaction_example(private_key, my_address):
         account_info.get('amount')) + "\n")
 
 
-first_transaction_example("bFiLvLhJsIec0+Ryo92jPzNYuHTJekPY0/lard+0qX7WoyiWDyeeVxjfE9QM6Xauj9RkaKq1yenEsuWcfMBUrA==",
-                          "22RSRFQPE6PFOGG7CPKAZ2LWV2H5IZDIVK24T2OEWLSZY7GAKSWKITNAC4")
+# replace private_key and my_address with your private key and your address.
+first_transaction_example("+EwYWXmA2RflodQt/cqZKuHFS9ROc6n1UHt+0lfcDvsPNxNf56zqK9G9MwVGrB6/sfaRzs5aXrcR0DgnulfOUw==",
+                          "B43RGX7HVTVCXUN5GMCUNLA6X6Y7NEOOZZNF5NYR2A4CPOSXZZJ3VPG3PA")
